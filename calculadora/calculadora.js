b1c2965830cf1c3619aeee01db342cca9cef3e67
joao <ribeiro.j@gsuite.iff.edu.br>
2025-07-18 11:32:19 -0300

    Update calculadora.js

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,1 +1,12 @@
+document.getElementById("forma").addEventListener("change", att)
 
+function calcularA() {
+    const forma = document.getElementById("forma").value
+    const dados = document.getElementById("dados")
+    dados.innerHTML = ""
+
+switch (forma) {
+    case 'Quadrado':
+        dados.innerHTML = "a"
+}
+}
